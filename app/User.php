067943bc490853d56389aff5cd7fc1c809bfa876
arfink <arf.at.sjv@gmail.com>
2016-04-29 19:54:41 -0600

    Eloquent model for signs, version 1

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function signs()
+    {
+        return $this->hasMany(Sign::class);
+    }
 }
