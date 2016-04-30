7103616eb907dab7f2b13be434940dffb14cbc0d
arfink <arf.at.sjv@gmail.com>
2016-04-29 22:49:07 -0600

    begin work on /signs page forms

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,10 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/task', 'TaskController@store');
     Route::delete('/task/{task}', 'TaskController@destroy');
 
+    Route::get('/signs', function() {
+    	return view('signs.index');
+    });
+
     Route::auth();
 
 });
